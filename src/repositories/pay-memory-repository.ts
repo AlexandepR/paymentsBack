451520dae7dc96bb67b3b5ab7d252b7474659296
Alex <alexandepp@gmail.com>
2022-06-29 11:40:09 +0300

    pay-router

--- src/repositories/pay-memory-repository.ts
+++ src/repositories/pay-memory-repository.ts
@@ -1,8 +1,7 @@
-
 export type PaymentsType = {
     RequestId: number, Amount: number
 }
-export let payments: PaymentsType[] = [{RequestId: 0,  Amount: 100  }]
+export let payments: PaymentsType[] = [{RequestId: 0, Amount: 100}]
 export let cards = [
     {CardNumber: '0000 0000 0000 0000', ExpDate: '04/2022', Cvv: '123'}
 ]
@@ -14,8 +13,7 @@ export const paysRepository = {
     postPayRouter(CardNumber: string, ExpDate: string, Cvv: string, Amount: number
     ) {
         const card = cards.find(c => c.CardNumber === CardNumber)
-        if (card && card.ExpDate === ExpDate && card.Cvv === Cvv)
-        {
+        if (card && card.ExpDate === ExpDate && card.Cvv === Cvv) {
             const newPay = {
                 RequestId: +(new Date()),
                 Amount: Amount
